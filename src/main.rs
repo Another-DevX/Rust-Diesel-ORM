4a3fda95f03914707b9c682b9e1a2eb0f62e8521
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-10-24 21:12:40 -0500

    feat: final details

--- src/main.rs
+++ src/main.rs
@@ -4,63 +4,114 @@ extern crate diesel;
 pub mod models;
 pub mod schema;
 
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use tera::Tera;
+
 use dotenvy::dotenv;
 use std::env;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
-use crate::schema::posts;
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+use diesel::r2d2::Pool;
+use diesel::r2d2::{self, ConnectionManager};
 
-fn main() {
+use self::models::{NewPost, NewPostHandler, Post, PostSimplified};
+use self::schema::posts;
+use self::schema::posts::dsl::*;
+
+#[get("/")]
+async fn index(template_manager: web::Data<tera::Tera>, pool: web::Data<DbPool>) -> impl Responder {
+    let mut conn = pool.get().expect("Error while connecting to DB");
+
+    match web::block(move || posts.load::<Post>(&mut conn)).await {
+        Ok(data) => {
+            println!("{:?}", data);
+
+            let data = data.unwrap();
+
+            let mut ctx = tera::Context::new();
+            ctx.insert("posts", &data);
+
+            HttpResponse::Ok()
+                .content_type("text/html")
+                .body(template_manager.render("index.html", &ctx).unwrap())
+        }
+        Err(err) => HttpResponse::InternalServerError().body("Error al recibir los datos."),
+    }
+}
+
+#[get("/post/{post_slug}")]
+async fn get_post(
+    template_manager: web::Data<tera::Tera>,
+    pool: web::Data<DbPool>,
+    post_slug: web::Path<String>,
+) -> impl Responder {
+    let mut conn = pool.get().expect("Error while connecting to DB");
+
+    let url_slug = post_slug.into_inner();
+
+    match web::block(move || posts.filter(slug.eq(url_slug)).load::<Post>(&mut conn)).await {
+        Ok(data) => {
+            println!("{:?}", data);
+
+            let data = data.unwrap();
+
+            if data.len() == 0 {
+                return HttpResponse::NotFound().body("Post not found");
+            }
+
+            let data = &data[0];
+
+            let mut ctx = tera::Context::new();
+            ctx.insert("post", &data);
+
+            HttpResponse::Ok()
+                .content_type("text/html")
+                .body(template_manager.render("post_template.html", &ctx).unwrap())
+        }
+        Err(err) => HttpResponse::InternalServerError().body("Error al recibir los datos."),
+    }
+}
+
+#[post("/new-post")]
+async fn new_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+    let mut conn = pool.get().expect("Error while connecting to DB");
+
+    println!("AQUI {:?}", item);
+
+    match web::block(move || Post::create_post(&mut conn, &item)).await {
+        Ok(data) => {
+            println!("{:?}", data);
+            return HttpResponse::Ok().body(format!("{:?}", data));
+        }
+        Err(err) => HttpResponse::InternalServerError().body("Error al recibir los datos."),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DB URL NOT FOUND");
 
-    let conn = &mut PgConnection::establish(&db_url).expect("Error while connecting to DB");
+    let connection = ConnectionManager::<PgConnection>::new(db_url);
 
-    use self::schema::posts::dsl::*;
-    use self::schema::posts;
-    use self::models::{Post, NewPost, PostSimplified};
+    let pool = Pool::builder()
+        .build(connection)
+        .expect("Failed to create pool");
 
-    // let new_post: NewPost<'_> = NewPost {
-    //     title: "Another title",
-    //     slug: "second-post",
-    //     body: "Hello world again!",
-    // };
+    HttpServer::new(move || {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
 
-    // let post: Post = diesel::insert_into(posts::table).values(&new_post).get_result(conn).expect("The insert failed");
-
-    println!("Query off limits");
-    let post_response = posts.load::<Post>(conn)
-        .expect("Error while executing query");
-
-    for post in post_response {
-        println!("{:?}", post)
-    }
-
-    
-    println!("Query with order by id");
-    let post_response = posts.order(id.desc()).load::<Post>(conn).expect("Error while executing query");
-
-    for post in post_response {
-        // println!("{}, {}", post.title, post.body)
-        println!("{:?}", post);
-    }
-
-    println!("Query with especific slug 'where'");
-    let post_response = posts.filter(slug.eq("second-post")).load::<Post>(conn).expect("Error while executing query");
-
-    for post in post_response {
-        // println!("{}, {}", post.title, post.body)
-        println!("{:?}", post);
-    }
-
-    println!("Query with especific columns");
-    let post_response = posts.select((title, body)).load::<PostSimplified>(conn).expect("Error while executing query");
-
-    for post in post_response {
-        // println!("{}, {}", post.title, post.body)
-        println!("{:?}", post);
-    }
-
+        App::new()
+            .service(index)
+            .service(new_post)
+            .service(get_post)
+            .app_data(web::Data::new(tera))
+            .app_data(web::Data::new(pool.clone()))
+    })
+    .bind(("127.0.0.1", 5173))?
+    .run()
+    .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,22 +1,28 @@
-#[derive(Queryable)]
-#[derive(Debug)]
+use diesel::{
+    r2d2::{ConnectionManager, PooledConnection},
+    PgConnection,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 #[diesel(table_name = crate::schema::posts)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct PostSimplified {
     pub title: String,
     pub body: String,
 }
-#[derive(Queryable)]
-#[derive(Debug)]
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 #[diesel(table_name = crate::schema::posts)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Post {
     pub id: i32,
-    pub slug: String,
     pub title: String,
+    pub slug: String,
     pub body: String,
 }
 
+
+use diesel::prelude::*;
 use super::schema::posts;
 
 #[derive(Insertable)]
@@ -25,4 +31,34 @@ pub struct NewPost<'a> {
     pub title: &'a str,
     pub slug: &'a str,
     pub body: &'a str,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
+    pub title: String,
+    pub body: String,
+}
+
+impl Post {
+    pub fn sluglify(title: &String) -> String {
+        return title.replace(" ", "-").to_lowercase();
+    }
+
+    pub fn create_post<'a> (
+        conn: &mut PgConnection, 
+        post: &NewPostHandler
+    ) -> Result<Post, diesel::result::Error> {
+
+        let slug = Post::sluglify(&post.title.clone());
+
+        println!("{}, {}, {}" , &post.title, &slug, &post.body);
+
+        let new_post = NewPost{
+            title: &post.title,
+            slug: &slug,
+            body: &post.body
+        };
+
+        diesel::insert_into(posts::table).values(new_post).get_result::<Post>(conn)
+    }
 }
